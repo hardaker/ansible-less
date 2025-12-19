24504c99967ba3af48220caee625ef6cd0a4a5e0
Wes Hardaker <opensource@hardakers.net>
2025-12-19 06:31:32 -0800

    ruff format

--- ansible_less/ansible_less.py
+++ ansible_less/ansible_less.py
@@ -10,7 +10,7 @@ import re
 
 # optionally use rich
 try:
-    #from rich import print
+    # from rich import print
     import rich
     from rich.logging import RichHandler
     from rich.theme import Theme
@@ -22,25 +22,35 @@ except Exception:
 help_handler = ArgumentDefaultsHelpFormatter
 try:
     from rich_argparse import RichHelpFormatter
+
     help_handler = RichHelpFormatter
 except Exception:
     debug("install rich_argparse for prettier help")
 
+
 def parse_args() -> Namespace:
     """Parse the command line arguments."""
-    parser = ArgumentParser(formatter_class=help_handler,
-                            description=__doc__,
-                            epilog="Example Usage: ")
+    parser = ArgumentParser(
+        formatter_class=help_handler, description=__doc__, epilog="Example Usage: "
+    )
 
-    parser.add_argument("-H", "--show-headers", action="store_true",
-                        help="Shows the top headers from the file too.")
+    parser.add_argument(
+        "-H",
+        "--show-headers",
+        action="store_true",
+        help="Shows the top headers from the file too.",
+    )
 
-    parser.add_argument("--log-level", "--ll", default="info",
-                        help="Define the logging verbosity level (debug, info, warning, error, fotal, critical).")
+    parser.add_argument(
+        "--log-level",
+        "--ll",
+        default="info",
+        help="Define the logging verbosity level (debug, info, warning, error, fotal, critical).",
+    )
 
-    parser.add_argument("input_file", type=FileType('r'),
-                        nargs='?', default=sys.stdin,
-                        help="")
+    parser.add_argument(
+        "input_file", type=FileType("r"), nargs="?", default=sys.stdin, help=""
+    )
 
     args = parser.parse_args()
     log_level = args.log_level.upper()
@@ -50,19 +60,23 @@ def parse_args() -> Namespace:
 
     # see if we're rich
     try:
-        handlers.append(RichHandler(rich_tracebacks=True,
-                                    tracebacks_show_locals=True,
-                                    console=Console(stderr=True,
-                                                    theme=Theme({"logging.level.success": "green"}))))
+        handlers.append(
+            RichHandler(
+                rich_tracebacks=True,
+                tracebacks_show_locals=True,
+                console=Console(
+                    stderr=True, theme=Theme({"logging.level.success": "green"})
+                ),
+            )
+        )
         datefmt = " "
         messagefmt = "%(message)s"
     except Exception:
         debug("failed to install RichHandler")
 
-    logging.basicConfig(level=log_level,
-                        format=messagefmt,
-                        datefmt=datefmt,
-                        handlers=handlers)
+    logging.basicConfig(
+        level=log_level, format=messagefmt, datefmt=datefmt, handlers=handlers
+    )
     return args
 
 
@@ -71,16 +85,16 @@ def group_by_hosts(lines: list[str]) -> dict[str, list[str]]:
     groupings = {}
     current_lines = []
     for line in lines:
-        if results := re.match(r'(changed|ok|failed): \[([^]]+)\]', line):
-            #print("FOUND: " + results.group(1) + " -- " + results.group(2))
+        if results := re.match(r"(changed|ok|failed): \[([^]]+)\]", line):
+            # print("FOUND: " + results.group(1) + " -- " + results.group(2))
             groupings[str(results.group(2))] = {
-                'status': str(results.group(1)),
-                'lines': current_lines
+                "status": str(results.group(1)),
+                "lines": current_lines,
             }
             current_lines = []
         else:
             current_lines.append(line)
-    #rich.print(groupings)
+    # rich.print(groupings)
     return groupings
 
 
@@ -100,8 +114,7 @@ def print_section(lines: list[str]) -> None:
 
     # print("------------------------")
     if strip_prefixes:
-        lines = [re.sub(r'^[^|]*\s*\| ', "", line) for line in lines]
-
+        lines = [re.sub(r"^[^|]*\s*\| ", "", line) for line in lines]
 
     if display_by_groups:
         task_line = lines.pop(0)
@@ -109,15 +122,15 @@ def print_section(lines: list[str]) -> None:
 
         buffer = []
         groupings = group_by_hosts(lines)
-        sorted_keys = sorted(groupings, key=lambda x: groupings[x]['lines'])
+        sorted_keys = sorted(groupings, key=lambda x: groupings[x]["lines"])
         last_key = None
         for key in sorted_keys:
-            status_line = "== " + groupings[key]['status'] + ": " + key + ":\n"
-            if last_key and groupings[last_key]['lines'] == groupings[key]['lines']:
+            status_line = "== " + groupings[key]["status"] + ": " + key + ":\n"
+            if last_key and groupings[last_key]["lines"] == groupings[key]["lines"]:
                 buffer.insert(-1, status_line)
                 continue
             buffer.append(status_line)
-            buffer.append("".join(groupings[key]['lines']))
+            buffer.append("".join(groupings[key]["lines"]))
             last_key = key
         print("".join(buffer))
     else:
@@ -157,7 +170,6 @@ def main():
         current_lines.append(line)
 
     printers[last_section](current_lines)
-            
 
 
 if __name__ == "__main__":
